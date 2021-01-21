86ebaaf167270e2167c799ab9d0ec49f2834cb51
Andreas Waschinski <an.andreas+github@posteo.de>
2021-01-21 23:18:37 +0100

    No longer git ignoring the assets folder

---
+++ assets/AppAsset.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+[           'href' => '/images/fez_pixelized.png',
+            'rel' => 'icon',
+            'sizes' => '512x512',
+        ],
+        [
+           'href' => '/images/fez_pixelized.png',
+            'rel' => 'apple-touch-icon-precomposed',
+        ],
+        'css/site.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
